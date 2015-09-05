cd5ecf6b310d8da7e6605670103fb0cc3f099ab3
Anton Farmar <11farmaa@TWGSBoys.kent.sch.uk>
2015-09-05 10:35:43 +0100

    Added FirstJust.hs
    FirstJust is a function which selects the first non-empty Maybe type from its arguments. Added examples.

---
+++ src/FirstJust.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
+class FirstReturn a r | r -> a where
+  fromMaybe :: Maybe a -> r
+
+instance FirstReturn a (Maybe a) where
+  fromMaybe = id
+
+instance (FirstReturn a r) => FirstReturn a (Maybe a -> r) where
+  fromMaybe Nothing a = fromMaybe a
+  fromMaybe a       _ = fromMaybe a
+
+firstJust :: (FirstReturn a r) => r
+firstJust = fromMaybe Nothing
+
+--  Examples:
+--> firstJust Nothing (Just 3) (Just 5) Nothing :: Maybe Int
+--> firstJust :: Maybe String
+--> firstJust (lookup a map) (lookup b map) (lookup c map) :: Maybe Something
