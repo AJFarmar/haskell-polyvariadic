e8f9c78c5c76300905c101e24f6dba434c036993
Anton Farmar <11farmaa@TWGSBoys.kent.sch.uk>
2015-09-23 18:32:12 +0100

    Created Monoids.hs
    Added examples.
    
    Interestingly, I need a type wrapper for this. I decided to make a 'Result' type so as to make it easier to understand.

---
+++ src/Monoids.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
+newtype Result a = Result {getResult :: a}
+  deriving (Show, Read, Eq, Ord)
+
+class MonoidReturn m r | r -> m where
+  fromMonoid :: m -> r
+
+instance MonoidReturn m (Result m) where
+  fromMonoid = Result
+
+instance (Monoid m, MonoidReturn m r) => MonoidReturn m (m -> r) where
+  fromMonoid a b = fromMonoid (a `mappend` b)
+  
+
+appends :: (Monoid m, MonoidReturn m r) => r
+appends = fromMonoid mempty
+
+--  Examples:
+--> getResult $ appends [1,2,3] [4,5] [6,7,8] [] [9,10] :: [Int]
+--> (getResult $ appends reverse id) [1,2,3,4,5] :: [Int]
+--> getResult $ appends (EQ, [1,2]) (GT, []) (GT, [1,2,3]) :: (Ordering, [Int])
